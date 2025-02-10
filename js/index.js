0745ed006f60b53b327ae1fdb5ed946fb18bf158
Sergio Galarza <sergiowgp@outlook.com>
2025-02-09 20:00:22 -0500

    pantalla de inicio

---
+++ js/index.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', async () => {
+    const config = await window.api.leerConfig();
+
+    if (config && config.rutaLogo) {
+        const imagen = document.getElementById('logo');
+        imagen.src = `file://${config.rutaLogo}`;
+    } else {
+        console.error('No se pudo cargar la imagen desde el JSON.');
+    }
+});
+
+document.getElementById('camaras').addEventListener('click', () => {
+    window.location.href = '../html/recognition.html';
+});
+
+document.getElementById('admi').addEventListener('click', () => {
+    window.location.href = '../html/login.html';
+});
